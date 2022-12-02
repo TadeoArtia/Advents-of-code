337bcb5dbe63dcbd467ab7a84e90843df3e02f01
Tadeo Artía <tlartia00@gmail.com>
2022-12-02 16:34:27 -0300

    part2

--- Advent 2/index.js
+++ Advent 2/index.js
@@ -20,34 +20,47 @@ let secret = new Map([
 	["Z", "C"],
 ]);
 
-function getRoundResult(move1, move2) {
+function getRoundResult1(move1, move2) {
+	let value = values.get(move2);
 	if (move1 == move2) {
-		return 3;
+		return value + 3;
 	}
-	if (move1 == "A") {
-		if (move2 == "B") {
-			return 6;
-		}
-		if (move2 == "C") {
-			return 0;
-		}
+	if (move1 == "A" && move2 == "B") {
+		return value + 6;
 	}
-	if (move1 == "B") {
-		if (move2 == "A") {
-			return 0;
-		}
-		if (move2 == "C") {
-			return 6;
-		}
+	if (move1 == "B" && move2 == "C") {
+		return value + 6;
 	}
-	if (move1 == "C") {
-		if (move2 == "A") {
-			return 6;
-		}
-		if (move2 == "B") {
-			return 0;
+	if (move1 == "C" && move2 == "A") {
+		return value + 6;
+	}
+	return value;
+}
+
+function getRoundResult2(move1, move2) {
+	if (move2 == "X") {
+		if (move1 == "A") {
+			return getRoundResult1(move1, "C");
+		} else if (move1 == "B") {
+			return getRoundResult1(move1, "A");
+		} else if (move1 == "C") {
+			return getRoundResult1(move1, "B");
 		}
 	}
+    else if (move2 == "Y") {
+        return getRoundResult1(move1, move1);
+    }
+    else{
+        if(move1 == "A"){
+            return getRoundResult1(move1, "B");
+        }
+        else if(move1 == "B"){
+            return getRoundResult1(move1, "C");
+        }
+        else if(move1 == "C"){
+            return getRoundResult1(move1, "A");
+        }
+    }
 }
 
 (function readData() {
@@ -58,21 +71,19 @@ function getRoundResult(move1, move2) {
 		play1.push(first);
 		play2.push(second);
 	}
-    console.log(play2);
+	console.log(play2);
 	assert(play1.length === play2.length);
 })();
 
 (function playGame() {
-	let result = 0;
+	let result1 = 0;
+	let result2 = 0;
 	for (let i = 0; i < play1.length; i++) {
 		let move1 = play1[i];
 		let move2 = secret.get(play2[i]);
-		let moveValue = values.get(move2);
-		let roundScore = getRoundResult(move1, move2);
-        console.log(move1);
-        console.log(move2);
-        result += moveValue + roundScore;
+		result1 += getRoundResult1(move1, move2);
+		result2 += getRoundResult2(move1, play2[i]);
 	}
-	console.log(result);
+	console.log(`Part 1 result: ${result1}`);
+	console.log(`Part 2 result: ${result2}`);
 })();
-
