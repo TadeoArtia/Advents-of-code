15f3190fd16b3b87522569c8c965a78263e1234b
Tadeo Artía <tlartia00@gmail.com>
2022-12-04 10:51:55 -0300

    add

--- Advent 4/index.js
+++ Advent 4/index.js
@@ -9,14 +9,14 @@ for (let i = 0; i < dataArr.length; i++) {
 	let intervals = dataArr[i]
 		.split(",")
 		.map((x) => x.split("-").map((y) => parseInt(y)));
-	acum1 += isFullOverlap(intervals[0], intervals[1]);
-	acum2 += getOverlapNumber(intervals[0], intervals[1]);
+	acum1 += isFullyOverlapped(intervals[0], intervals[1]);
+	acum2 += isOverlapped(intervals[0], intervals[1]);
 }
 
 console.log(`Result fisrst part: ${acum1}`);
 console.log(`Result second part: ${acum2}`);
 
-function isFullOverlap(interval1, interval2) {
+function isFullyOverlapped(interval1, interval2) {
 	if (interval1[0] <= interval2[0]) {
 		if (interval1[1] >= interval2[1]) {
 			return 1;
@@ -30,7 +30,7 @@ function isFullOverlap(interval1, interval2) {
 	return 0;
 }
 
-function getOverlapNumber(interval1, interval2) {
+function isOverlapped(interval1, interval2) {
 	//swap 1 always starts before.
 	if (interval1[0] >= interval2[0]) {
 		let aux = interval1;
